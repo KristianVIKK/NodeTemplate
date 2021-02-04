92961e6f9776eb28354c68e78f7c9b42ef12f74b
Kristian Kõiv <kristian.koiv@vikk.ee>
2021-02-04 12:21:18 +0200

    test

---
+++ src/components/Orders.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2 class="mb-3">Tellimused</h2>
+        <b-table striped hover :items="items"></b-table>
+    <!-- <p>Count: {{count}}</p>
+    <button @click="addCount()">+</button>
+    <button @click="removeCount()">-</button> -->
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+
+export default {
+  name: 'Orders',
+  data() {
+    return {
+      
+      count: 0,
+      
+        fields: ['orderID', 'toWhom', 'toCity'],
+        items: [],
+    }
+  },
+  async created () {
+    const orders = await axios({
+      url: 'api/orders',
+      method: 'GET',
+      headers: {}
+    })
+    console.log('orders', orders)
+
+    this.items = orders.data.allOrders
+  },
+  methods: {
+    addCount () {
+      console.log('Praegune count:', this.count)
+      this.count++
+    },
+    removeCount () {
+      console.log('Praegune count:', this.count)
+      this.count--
+
+      
+    }
+  }
+}
+</script>
