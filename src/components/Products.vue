07984fd2214a1a4626ef78aaa1864a054744e695
Kristian Kõiv <kristian.koiv@vikk.ee>
2021-02-07 19:21:21 +0200

    Tooted

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,25 +1,77 @@
 <template>
-<div>
-<h2>Tooted</h2>
+  <div>
+    <h2 class="mb-3">Tooted</h2>
+       <!-- Toodete tabel -->
+    <b-table striped hover :items="items" :fields="fields">
+
+    
+
+      <!-- <template #cell(actions)="data">
+        <b-button v-b-modal.modal-1 variant="success" @click="showProducts(data.item.products, data.item)">Vaata tooteid</b-button>
+      </template> -->
+
+    </b-table>
+
+    <!-- Toote tabel -->
+    <!-- TODO: Add real data -->
+    
+    <!-- <p>Count: {{count}}</p>
+    <button @click="addCount()">+</button>
+    <button @click="removeCount()">-</button> -->
+
+ 
+    <b-table striped hover :items="items">
+      <template #cell(productPrice)="data">
+        <b class="text-info">{{ data.productPrice }} EUR</b>
+      </template>
+    </b-table>
+  
 
 
-
-</div>
+  </div>
 </template>
-
 <script>
+
+import axios from 'axios'
+
+
+
 export default {
-    data () {
-        return {
+  name: 'Products',
+  data() {
+    return {
+      
+      count: 0,
+      
+      
 
-        }
-    },
-    async created() {
+      fields: [{key:'_id', label: 'Toote ID'}, {key: 'productName', label: 'Tootenimi'}, {key: 'productCat', label: 'Kategooria'}, 
+      {key: 'productQty', label: 'Kogus'}, {key: 'productDescription', label: 'Tootekirjeldus'}, { key: 'productPrice', label: 'Hind' }],
+      items: [],
+      
+     
+    }
+  },
+  async created () {
+    const products = await axios({
+      url: 'api/products',
+      method: 'GET',
+      headers: {}
+    })
+    
 
-    },
-    methods: {
+    this.items = products.data.allProducts
 
+  },
+  methods: {
+    showProducts (products) {
+      
+      this.productItems = products
+      
     }
 
-}
+      
+    }
+  }
+
 </script>
